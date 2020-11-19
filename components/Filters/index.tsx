10c16e7783aca77600cb68008466bb6829845174
Fernando Chavez <fc9chavez@gmail.com>
2020-11-19 18:34:22 -0400

    Mobile Responsive design in searchPage

--- components/Filters/index.tsx
+++ components/Filters/index.tsx
@@ -4,7 +4,7 @@ import { Restaurant } from "./Restaurant"
 
 export const Filters = () => {
   return (
-    <div className="border border-thyellow md:w-64 mx-auto rounded-lg shadow-md py-4">
+    <div className="md:border md:border-thyellow md:w-64 mx-auto rounded-lg md:shadow-md py-4">
       <FoodType />
       <Restaurant />
       <PriceRange />
--- pages/buscar/[[...filters]].tsx
+++ pages/buscar/[[...filters]].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { SimpleButton } from "../../components/Buttons/SimpleButton"
 import { FiltersProvider, RESTAURANT_OPTIONS, TYPE_OPTIONS, useFilters } from "../../components/Contexts/Filters/context"
 import { Filters } from "../../components/Filters"
@@ -292,6 +292,7 @@ const PRODUCTS_DUMMY = [
 
 const SearchPage = ({ filters: urlFilters }) => {
   const { filters, dispatch } = useFilters()
+  const [showFilter, setShowFilter] = useState(false)
 
   useEffect(() => {
     if(urlFilters){
@@ -304,10 +305,21 @@ const SearchPage = ({ filters: urlFilters }) => {
 
   return (
     <div className="flex">
-      <div className="w-3/12 hidden md:block">
-        <Filters />
+      <div className={`w-full md:w-3/12 fixed md:relative md:block z-30 md:z-20 bg-white top-0 h-screen ${showFilter ? "flex flex-col" : "hidden"}`}>
+        <div className="p-4 border-b md:hidden">
+          <p className="text-xl font-bold">Filtros</p>
+        </div>
+        <div className="flex-grow overflow-y-auto">
+          <Filters />
+        </div>
+        <div className="p-4 border-t md:hidden">
+          <button className="button-with-gradient w-full h-8 rounded-lg" onClick={() => setShowFilter(false)}>Ver +99 resultados</button>
+        </div>
       </div>
       <div className="w-full md:w-9/12 px-4">
+        <div className="md:hidden flex p-2 border-b z-30">
+          <button className="button-with-gradient w-full h-8 rounded-lg" onClick={() => setShowFilter(true)}>Filtros</button>
+        </div>
         <SearchResults products={PRODUCTS_DUMMY} />
       </div>
     </div>
